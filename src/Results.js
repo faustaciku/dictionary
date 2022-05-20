bfc71a076e25b24242b443933701a66891ec58e1
faustaciku <faustaciku@gmail.com>
2022-05-20 15:01:37 +0100

    Added phonetics using API

--- src/Results.js
+++ src/Results.js
@@ -7,7 +7,13 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        <Phonetics props={props.results.phonetics} />
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
