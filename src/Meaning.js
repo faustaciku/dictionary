37014668ac4e1591da5fdbd1481668901e265f5b
faustaciku <faustaciku@gmail.com>
2022-05-20 14:46:24 +0100

    Added synonims using API

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,18 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
+            <p>{definition.definition}</p>
             <p>
-              {definition.definition}
-              <br />
               <em>{definition.example}</em>
             </p>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
