a0f86efcb7d857e22ede32c6cee324139d2c9021
faustaciku <faustaciku@gmail.com>
2022-05-20 22:40:33 +0100

    Added photos using API intergration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("like");
+  let [keyword, setKeyword] = useState("sunrise");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
   function handleResponse(response) {
     setResults(response.data[0]);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000195acc268d07b4d40bbe173809989a678";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -27,16 +38,19 @@ export default function Dictionary() {
   }
   if (loaded) {
     return (
-      <div className="Dictionary">
-        <section>
-          <h2>What word do you want to look up?</h2>
+      <div className="container">
+        <div className="Dictionary">
+          <section>
+            <h2>What word do you want to look up?</h2>
 
-          <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} />
-          </form>
-          <div className="suggestion">i.e. love, sunset, coding, yoga</div>
-        </section>
-        <Results results={results} />
+            <form onSubmit={handleSubmit}>
+              <input type="search" onChange={handleKeywordChange} />
+            </form>
+            <div className="suggestion">i.e. love, sunset, coding, yoga</div>
+          </section>
+          <Results results={results} />
+          <Photos photos={photos} />
+        </div>
       </div>
     );
   } else {
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,7 +3,7 @@ import ReactAudioPlayer from "react-audio-player";
 export default function Phonetics(props) {
   if (props.phonetic && props.phonetic.audio) {
     return (
-      <div className="Phonetics">
+      <div className="Phonetics mt-1">
         {props.phonetic.text}
         <br />
         <ReactAudioPlayer src={props.phonetic.audio} controls />
